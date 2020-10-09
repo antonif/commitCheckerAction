53465e358cb0bac0a805493c4ca66e94f7c53e05
Felix Antoni <felix.antoni@p92.hu>
2020-10-09 10:11:11 +0200

    GRA-12345 - Web Test - Asdfg asdF F

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Próba..");
         System.out.println("Test in main branch");
+        System.out.println("Test after merging .GitHub/Workflow with main");
     }
 }
