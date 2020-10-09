33bed12e0343a309dcd1f4a5f24537572b89ceca
Felix Antoni <felix.antoni@p92.hu>
2020-10-09 11:07:38 +0200

    Asdf - Web Facade - GRA-12345

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Test after merging .GitHub/Workflow with main..");
         System.out.println("TestV2");
         System.out.println("Test With Wrong Commit Message Format");
+        System.out.println("Test with right format");
     }
 }
