3cc0867553a94da23d46a485c7d85b8bba9ad1ef
Felix Antoni <felix.antoni@p92.hu>
2020-10-09 10:08:11 +0200

    RPS-12345 - akarmi comes here - the rest of the text

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Próba..");
+        System.out.println("Test in main branch");
     }
 }
