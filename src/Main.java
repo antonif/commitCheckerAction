0a065afdd945b7246f7d18b786f798f79fb7121f
Felix Antoni <felix.antoni@p92.hu>
2020-10-14 11:39:18 +0200

    GRA-12345 - Web Facade - Test 1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Próba..");
-        System.out.println("Test in main branch");
-        System.out.println("Test after merging .GitHub/Workflow with main..");
-        System.out.println("TestV2");
-        System.out.println("Test With Wrong Commit Message Format");
+        System.out.println("Test with src files in repo");
     }
 }
