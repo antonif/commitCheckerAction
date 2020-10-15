020e2e61d91612abf5661e196535115635020b81
Felix Antoni <felix.antoni@p92.hu>
2020-10-15 11:47:13 +0200

    GRA-12345 - Web Facade - Test123

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test2 with src files in repo");
+        System.out.println("Test3 with working repo");
     }
 }
