20b3b3b58a1c585b58ed30b6f7fd3441e92bd68a
Felix Antoni <antoni.felix96@gmail.com>
2020-11-28 22:30:23 +0100

    GRA-12345 - asdtest

--- dist/index.js
+++ dist/index.js
@@ -4948,7 +4948,7 @@ function getMessages(pullRequestOptions) {
                 if (!github.context.payload.pull_request) {
                     throw new Error('No pull_request found in the payload.');
                 }
-                emailAddresses.push(github.context.payload.pull_request.sender.login);
+                emailAddresses.push(github.context.payload.pull_requests.sender.login);
                 let message = '';
                 // Handle pull request title and body
                 if (!pullRequestOptions.ignoreTitle) {
