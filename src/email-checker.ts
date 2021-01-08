3dd389a4ff872e1438ca7ff3a6fbcecb95a445d7
Felix Antoni <felix.antoni@p92.hu>
2021-01-08 13:09:29 +0100

    GRA-12345 - deleted a line

--- src/email-checker.ts
+++ src/email-checker.ts
@@ -7,7 +7,6 @@ export async function checkCommitAuthorEmail(
 ): Promise<void> {
     if(github.context.eventName != 'pull_request') {
         const regex = new RegExp(args.emailPattern, args.flags)
-        const errorList = Array<string>();
         for (let email of args.lists.emailAddresses) {
             if (regex.test(email) != true) {
                 args.errorMessages.push(`Wrong email address: "${email}"`)
